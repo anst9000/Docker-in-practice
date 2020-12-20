ee937f3160975f0c9c2846830746967fcd053261
anst9000 <coltlaboy@yahoo.se>
2020-12-20 11:58:53 +0100

    Safer environment variables handling

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 ## demo app - developing with Docker
 
-This demo app shows a simple user profile app set up using 
+This demo app shows a simple user profile app set up using
+
 - index.html with pure js and css styles
 - nodejs backend with express mode
 - mongodb for data storage
@@ -12,31 +13,34 @@ All components are docker-based
 Step 1: start mongodb and mongo-express
 
     docker-compose -f docker-compose.yaml up
-    
+
 _You can access the mongo-express under localhost:8080 from your browser_
-    
+
 Step 2: in mongo-express UI - create a new database "my-db"
 
-Step 3: in mongo-express UI - create a new collection "users" in the database "my-db"       
-    
-Step 4: start node server 
+Step 3: in mongo-express UI - create a new collection "users" in the database "my-db"
+
+Step 4: start node server
 
     node server.js
-    
+
 _You can access the application under localhost:3000 from your browser_
 
 #### To build a docker image from the application
 
-    docker build -t my-app:1.0 .       
-    
+    docker build -t my-app:1.0 .
+
 The dot "." at the end of the command denotes location of the Dockerfile.
 
 # Commands used
 
 ## Create docker network
+
 docker network create mongo-network
 
 ## Start mongodb
+
+```
 docker run -d \
 -p 27017:27017 \
 -e MONGO_INITDB_ROOT_USERNAME=$USERNAME \
@@ -44,9 +48,11 @@ docker run -d \
 --net mongo-network \
 --name mongodb \
 mongo
-
+```
 
 ## Start mongo-express
+
+```
 docker run -d \
 -p 8081:8081 \
 -e ME_CONFIG_MONGODB_ADMINUSERNAME=$USERNAME \
@@ -55,4 +61,12 @@ docker run -d \
 --net mongo-network \
 --name mongo-express \
 mongo-express
+```
 
+## Running Docker Compose with environment file
+
+> https://docs.docker.com/compose/environment-variables/
+
+```
+docker-compose --env-file ./config/.env.dev up
+```
--- app/server.js
+++ app/server.js
@@ -1,68 +1,78 @@
-let express = require('express');
-let path = require('path');
-let fs = require('fs');
-let MongoClient = require('mongodb').MongoClient;
-let bodyParser = require('body-parser');
+let express = require("express");
+let path = require("path");
+let fs = require("fs");
+let MongoClient = require("mongodb").MongoClient;
+let bodyParser = require("body-parser");
 let app = express();
+require("dotenv").config();
 
-let username = 'acke';
-let password = 'ksbv27';
+let username = process.env.MONGO_USERNAME;
+let password = process.env.MONGO_PASSWORD;
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
-  });
-
-app.get('/profile-picture', function (req, res) {
-  let img = fs.readFileSync(path.join(__dirname, "images/profile-1.jpg"));
-  res.writeHead(200, {'Content-Type': 'image/jpg' });
-  res.end(img, 'binary');
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
+app.get("/profile-picture", function (req, res) {
+  let img = fs.readFileSync(path.join(__dirname, "images/profile-1.jpg"));
+  res.writeHead(200, { "Content-Type": "image/jpg" });
+  res.end(img, "binary");
+});
+
+console.log("username is", username);
+console.log("password is", password);
+
 // use when starting application locally
-let mongoUrlLocal = `mongodb://acke:ksbv27@127.0.0.1:27017`;
-
+let mongoUrlLocal = `mongodb://${username}:${password}@127.0.0.1:27017`;
+console.log(mongoUrlLocal);
 // use when starting application as docker container
-let mongoUrlDocker = `mongodb://acke:ksbv27@mongodb`;
+let mongoUrlDocker = `mongodb://${username}:${password}@mongodb`;
 
-app.post('/update-profile', function (req, res) {
+app.post("/update-profile", function (req, res) {
   let userObj = req.body;
 
   MongoClient.connect(mongoUrlLocal, function (err, client) {
     if (err) {
-      console.log('Some error with connection');
+      console.log("Some error with connection");
       throw err;
     }
 
-    console.log('Seem to be connected...');
+    console.log("Seem to be connected...");
 
-    let db = client.db('user-account');
-    userObj['userid'] = 1;
+    let db = client.db("user-account");
+    userObj["userid"] = 1;
 
     let myquery = { userid: 1 };
     let newvalues = { $set: userObj };
 
-    db.collection("users").updateOne(myquery, newvalues, {upsert: true}, function(err, res) {
-      if (err) throw err;
-      client.close();
-    });
-
+    db.collection("users").updateOne(
+      myquery,
+      newvalues,
+      { upsert: true },
+      function (err, res) {
+        if (err) throw err;
+        client.close();
+      }
+    );
   });
   // Send response
   res.send(userObj);
 });
 
-app.get('/get-profile', function (req, res) {
+app.get("/get-profile", function (req, res) {
   let response = {};
   // Connect to the db
   MongoClient.connect(mongoUrlLocal, function (err, client) {
     if (err) throw err;
 
-    let db = client.db('user-account');
+    let db = client.db("user-account");
 
     let myquery = { userid: 1 };
 
